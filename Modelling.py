0edfda2bc0f40a052e153f1df89606ef3899c2a5
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-06 11:06:22 -0600

    Imported correctly Score Dataset

--- Modelling.py
+++ Modelling.py
@@ -2,11 +2,23 @@
 # SUPERVISED MODELLING USING THE UNSUPERVISED DB
 #######################
 
-# I.- IMPORT DATASET WHICH IS LOCATED INSIDE A WINRAR FILE
+import time
+start_time = time.time()
+
+# I.- IMPORT DATASET WHICH IS LOCATED INSIDE A GZIP FILE
 
 import pandas as pd
 import numpy as np
+pd.set_option('display.max_columns',500)
 
 v_path = 'Z:/Modelos BA/1. 14Nov18 - Indexing/compiled_scored.gz'
 
 df = pd.read_csv(v_path, compression = 'gzip')
+
+print(df.shape)
+print(list(df.columns))
+print(df.head())
+
+###############################################
+print('\n\nTime:'+str(time.time()-start_time))
+###############################################
