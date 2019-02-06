82e0e4faa06916ee6ff8e26f9fadcee683de27d1
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-06 16:45:29 -0600

    Model Score for only Logistic Regression

--- Modelling.py
+++ Modelling.py
@@ -26,11 +26,20 @@ ls_contVariables = [c for c in df if c not in ls_indexVariables+target]
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 
 X_train, X_test, y_train, y_test = train_test_split(df[ls_contVariables],df[target],
 test_size=.7, stratify=df[target])
 
 model = LogisticRegression().fit(X_train,y_train)
+
+### MODEL SCORING
+
+print('Train Model Score:')
+print(roc_auc_score(y_train,model.predict(X_train)))
+
+print('Test Model Score:')
+print(roc_auc_score(y_test,model.predict(X_test)))
 
 ###############################################
 print('\n\n Time:'+str(time.time()-start_time))
