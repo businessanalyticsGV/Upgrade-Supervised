0c6ae85e80fedc1839139f711eb79c8eca7ebe65
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-06 13:35:06 -0600

    Added Target Variable

--- Modelling.py
+++ Modelling.py
@@ -11,10 +11,14 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
+pd.set_option('display.max_columns',500)
+
 df = pd.read_csv('dataframe.csv')
 
+### CREATED TARGET VARIABLE
+df['upgrade'] = [1 if df.iloc[i][16] == 1 else 0 for i in range(df.shape[0])][1:]+[0]
+
 print(df.shape)
-print(df.head())
 
 ###############################################
 print('\n\nTime:'+str(time.time()-start_time))
