aab8f784681b49199ada9ce30a8fa6aad5506b80
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-06 17:57:20 -0600

    Added more techniques to the modelling processes

--- Modelling.py
+++ Modelling.py
@@ -25,21 +25,37 @@ target = ['upgrade']
 ls_contVariables = [c for c in df if c not in ls_indexVariables+target]
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 X_train, X_test, y_train, y_test = train_test_split(df[ls_contVariables],df[target],
 test_size=.7, stratify=df[target])
 
-model = LogisticRegression().fit(X_train,y_train)
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
+dic_models = {'Logistic Regression':LogisticRegression().fit(X_train,y_train),
+'Random Forest':RandomForestClassifier().fit(X_train,y_train),
+'Neural Network Classifier': MLPClassifier().fit(X_train,y_train),
+'Discriminant Analysis':QuadraticDiscriminantAnalysis().fit(X_train,y_train)
+# 'Suppor Vector Machine':SVC(kernel="linear", C=0.025).fit(X_train,y_train)}
+}
 ### MODEL SCORING
+for model in dic_models:
+    print('\n\n++'+model)
 
-print('Train Model Score:')
-print(roc_auc_score(y_train,model.predict(X_train)))
+    score = roc_auc_score(y_train,dic_models[model].predict(X_train))
+    print('Train Model Score:'+str(score))
 
-print('Test Model Score:')
-print(roc_auc_score(y_test,model.predict(X_test)))
+    score = roc_auc_score(y_test,dic_models[model].predict(X_test))
+    print('Test Model Score:'+str(score))
 
 ###############################################
 print('\n\n Time:'+str(time.time()-start_time))
